47361bfb34bc2fad92a329e6392d977118e38710
LostYouthgit <you@example.com>
2018-06-23 22:59:01 +0300

    nyam

---
+++ StupidGame/Assets/Scripts/PlayBeh.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayBeh : MonoBehaviour {
+
+	public float speed = 10f;
+	private Rigidbody2D rb;
+	
+
+	// Use this for initialization
+	void Start () {
+	
+		rb = GetComponent <Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			float moveX = Input.GetAxis("Horizontal");
+			rb.MovePosition (rb.position + Vector2.right * moveX * speed * Time.deltaTime);
+			
+			if (Input.GetKeyDown (KeyCode.Space))
+				Jump();
+	}
+
+	void FixedUpdate()
+	{
+		
+				//rb.AddForce (transform.up * 3f, ForceMode2D.Impulse);
+	}
+
+	void Jump()
+	{
+		rb.AddForce (transform.up * 70f, ForceMode2D.Impulse);
+	}
+
+	void OnCollisionEnter2D (Collision2D touch)
+	{
+		if (touch.gameObject.tag == "Respawn")
+			ReloadLvl();
+	}
+
+	void OnTouch2D (Collision2D touch)
+	{
+		if (touch.gameObject.tag == "Finish")
+			Exit();
+	}
+
+	void Exit ()
+	{
+		Application.Quit();
+	}
+
+	void ReloadLvl ()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+
+	}
+
+
+}
---
+++ StupidGame/Assets/Scripts/Reload.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Reload : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollision (Collision2D touch)
+	{
+		if (touch.gameObject.tag == "Respawn")
+			ReloadLvl();
+	}
+
+	void ReloadLvl ()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+
+	}
+
+}
